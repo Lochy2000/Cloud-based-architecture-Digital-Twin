a565ba4d7327d2d4df4bcf5b07ff4d9422ff75d7
lochy2000 <lochlann_oht@hotmail.com>
2026-08-26 17:13:12 +0100

    ymal structure

--- src/twin/assets.py
+++ src/twin/assets.py
@@ -10,3 +10,14 @@ import yaml
 
 class AssetError(Exception):
     """Raised when an asset configuration file is missing or malformed."""
+
+REQUIRED_STRUCTURE = {
+    "steady_state": ("supply_temperature", "return_temperature", "ambient_baseline"),
+    "dynamics": ("heating_time_constant_seconds", "cooling_time_constant_seconds"),
+    "duty_cycle": (
+        "operating_hours_start",
+        "operating_hours_end",
+        "cycle_period_seconds",
+        "on_fraction",
+    ),
+}
